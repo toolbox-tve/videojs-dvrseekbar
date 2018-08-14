406855c94dd5f918bb2b2a38ed07d56d7aba8f89
Luciano <lribelle@tbxnet.com>
2018-08-14 15:09:53 -0300

    fix(liveButton): Use of this.player and show Live if displayTime is Infinity

--- src/components/LiveButton/liveButton.js
+++ src/components/LiveButton/liveButton.js
@@ -15,25 +15,26 @@ class LiveButton extends Component {
   createEl() {
     return videojs.dom.createEl('div', {
       id: 'dvr-current-time',
-      className: 'vjs-current-time-display',
+      // Starts hidden
+      className: 'vjs-current-time-display vjs-hidden',
       innerHTML: '0:00'
     });
   }
 
   clicked() {
     // Jump to live
-    player.currentTime(getSeekRange(this.player()).end);
+    this.player_.currentTime(getSeekRange(this.player_).end);
   }
 
   update() {
-    const seekRange = getSeekRange(player);
+    const seekRange = getSeekRange(this.player_);
     const showHour = (seekRange.end - seekRange.start) >= 3600;
-    const displayTime = behindLiveTime(this.player());
+    const displayTime = behindLiveTime(this.player_);
 
     // Consider "LIVE" when less than 15 second behind the live-edge.  Always
     // show the full time string when seeking, including the leading '-';
     // otherwise, the time string "flickers" near the live-edge.
-    if (displayTime >= SHOW_LIVE_MAX) {
+    if (displayTime !== Infinity && displayTime >= SHOW_LIVE_MAX) {
       this.el().innerHTML = `- ${buildTimeString(displayTime, showHour)}`;
     } else {
       this.el().innerHTML = this.localize('LIVE');
